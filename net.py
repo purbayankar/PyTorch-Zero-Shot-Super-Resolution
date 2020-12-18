0d0606f11893604f1391dfc8083383195d98b7f5
purbayankar <53092711+purbayankar@users.noreply.github.com>
2020-12-18 22:33:49 +0530

    Update net.py

--- net.py
+++ net.py
@@ -28,24 +28,22 @@ class SRNet(nn.Module):
 		x = self.relu(self.Conv1(LR_img))
 		x_ = self.relu(self.Conv1_(self.pad(LR_img)))
 		x = torch.cat((x,x_), dim=1)
-		print(x.shape)
-		x = self.relu(self.Conv2(x))
-# 		x_ = self.relu(self.Conv2_(self.pad(x)))
-# 		x = torch.cat((x,x_), dim=1)
-		x = self.relu(self.Conv3(x))
-		print(x.shape)
-		x = self.relu(self.Conv4(x))
-		x = x + LR_img
-		x = self.relu(self.Conv5(x))
-		x_ = self.relu(self.Conv5_(self.pad(x)))
-		x = torch.cat((x,x_), dim=1)
-		x = self.relu(self.Conv6(x))
-# 		x_ = self.relu(self.Conv6_(self.pad(x)))
-# 		x = torch.cat((x,x_), dim=1)
-		x = self.relu(self.Conv7(x))
-		x = self.Conv8(x)
+		x1 = self.relu(self.Conv2(x))
+		x1_ = self.relu(self.Conv2_(self.pad(x)))
+		x2 = torch.cat((x1,x1_), dim=1)
+		x2 = self.relu(self.Conv3(x2))
+		x2 = self.relu(self.Conv4(x2))
+		x2 = x2 + LR_img
+		x3 = self.relu(self.Conv5(x2))
+		x3_ = self.relu(self.Conv5_(self.pad(x2)))
+		x4 = torch.cat((x3,x3_), dim=1)
+		x5 = self.relu(self.Conv6(x4))
+		x5_ = self.relu(self.Conv6_(self.pad(x4)))
+		x6 = torch.cat((x5,x5_), dim=1)
+		x6 = self.relu(self.Conv7(x6))
+		x6 = self.Conv8(x6)
 
-		SR_img = LR_img + x     # Because we have to learn residuals.
+		SR_img = LR_img + x6     # Because we have to learn residuals.
 
 		return SR_img
 
