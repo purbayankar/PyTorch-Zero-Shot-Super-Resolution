fc2599fb544fc606ba5273472172e9ffe651d659
purbayankar <53092711+purbayankar@users.noreply.github.com>
2020-12-19 02:29:19 +0530

    Update net.py

--- net.py
+++ net.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 import math
-from attention_augmented_conv import AugmentedConv
+# from attention_augmented_conv import AugmentedConv
 
-use_cuda = torch.cuda.is_available()
-device = torch.device('cuda' if use_cuda else 'cpu')
+# use_cuda = torch.cuda.is_available()
+# device = torch.device('cuda' if use_cuda else 'cpu')
 
 class SRNet(nn.Module):
 
@@ -25,8 +25,8 @@ class SRNet(nn.Module):
 		self.Conv6 = nn.Conv2d(128,64,3,1,1,bias=True)
 		self.Conv6_ = nn.Conv2d(128,64,5,1,1,bias=True)
 		self.Conv7 = nn.Conv2d(128,64,3,1,1,bias=True)
-# 		self.Conv8 = nn.Conv2d(64,3,3,1,1,bias=True)
-		self.Conv8 = AugmentedConv(in_channels=64, out_channels=3, kernel_size=3, dk=20, dv=2, Nh=2, relative=False, stride=1).to(device)
+		self.Conv8 = nn.Conv2d(64,3,3,1,1,bias=True)
+# 		self.Conv8 = AugmentedConv(in_channels=64, out_channels=3, kernel_size=3, dk=20, dv=2, Nh=2, relative=False, stride=1).to(device)
 
 	def forward(self, LR_img):
 		
