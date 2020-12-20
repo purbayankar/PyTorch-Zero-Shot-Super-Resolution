8a3894c45c58e50dd7d294cecf540bf222df02a1
purbayankar <53092711+purbayankar@users.noreply.github.com>
2020-12-20 23:15:59 +0530

    Update net.py

--- net.py
+++ net.py
@@ -52,7 +52,7 @@ class SRNet(nn.Module):
 		x6 = self.Conv8(x6)
 		x6 = self.eca(x6)
 
-		SR_img = LR_img + x6     # Because we have to learn residuals.
+		SR_img = LR_img + x2     # Because we have to learn residuals.
 
 		return SR_img
 
